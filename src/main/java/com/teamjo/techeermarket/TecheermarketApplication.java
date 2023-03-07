4f018c4a5fd4e1102831e525342af020563659bd
Dayeon-Hong <dayon0325@gmail.com>
2023-03-07 16:23:41 +0900

    (TM-01) backend setting

---
+++ src/main/java/com/teamjo/techeermarket/TecheermarketApplication.java
@@ -0,0 +1,13 @@
+package com.teamjo.techeermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamjo/techeermarket/TecheermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamjo.techeermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
