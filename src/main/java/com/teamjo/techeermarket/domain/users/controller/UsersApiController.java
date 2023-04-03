38be9e74bdd6448151758ae8fb665e0200224e9b
soohyun <vivian0304@naver.com>
2023-04-04 04:23:18 +0900

    (TM-16) 회원가입 프로필 업로드 (B)

--- src/main/java/com/teamjo/techeermarket/domain/users/controller/UsersApiController.java
+++ src/main/java/com/teamjo/techeermarket/domain/users/controller/UsersApiController.java
@@ -1,14 +1,19 @@
 package com.teamjo.techeermarket.domain.users.controller;
 
+import com.teamjo.techeermarket.domain.users.dto.request.UsersLoginRequestDto;
 import com.teamjo.techeermarket.domain.users.dto.request.UsersRequestDto;
 import com.teamjo.techeermarket.domain.users.dto.response.UsersResponseDto;
 import com.teamjo.techeermarket.domain.users.entity.Users;
 import com.teamjo.techeermarket.domain.users.mapper.UsersMapper;
 import com.teamjo.techeermarket.domain.users.service.UsersApiService;
+//import com.teamjo.techeermarket.global.security.JwtToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,13 +35,11 @@ public class UsersApiController {
         return usersMapper.fromEntity(users);
     }
 
-//    @PostMapping("/signup")
-//    public UsersResponseDto update(@ModelAttribute UsersRequestDto usersRequestDto) {
-//        log.info(usersRequestDto.toString());
-//        if (usersRequestDto == null) {
-//            return null;
-//        }
-//        Users users = usersApiService.signup(usersRequestDto);
-//        return usersMapper.fromEntity(users);
+//    @PostMapping("/login")
+//    public ResponseEntity<JwtToken> loginSuccess(@ModelAttribute UsersLoginRequestDto loginForm) {
+//        log.info(loginForm.toString());
+//        JwtToken token = usersApiService.login(loginForm.getEmail(), loginForm.getPassword());
+//        return ResponseEntity.ok(token);
 //    }
+
 }
