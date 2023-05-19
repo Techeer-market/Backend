f1793de45e9d77121c2bd1f5779555b805d2f5d4
Dayeon-Hong <dayon0325@gmail.com>
2023-05-20 04:53:41 +0900

    global로 에러 코드 만들려다 미완.

---
+++ src/main/java/com/teamjo/techeermarket/global/error/ConflictException.java
@@ -0,0 +1,7 @@
+package com.teamjo.techeermarket.global.error;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
