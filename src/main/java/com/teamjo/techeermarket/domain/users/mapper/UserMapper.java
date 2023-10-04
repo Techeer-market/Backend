95b1c99fb11de2f813c447d4fc4c842990611af6
Dayeon-Hong <dayon0325@gmail.com>
2023-10-04 21:48:36 +0900

    feat: 회원가입, 로그인 코드 추가 #27

---
+++ src/main/java/com/teamjo/techeermarket/domain/users/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.teamjo.techeermarket.domain.users.mapper;
+
+import com.teamjo.techeermarket.domain.users.dto.SignUpRequestDto;
+import com.teamjo.techeermarket.domain.users.entity.Users;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    Users toEntity(SignUpRequestDto dto);
+    SignUpRequestDto toDto(Users entity);
+
+}
+
